123772a91a735bbb4742005af91209e72d76fdf6
Arsenii Stratiuk <stratuik.777@gmail.com>
2024-11-16 11:11:02 +0200

    Upload main file, input example

---
+++ main.py
@@ -0,0 +1,47 @@
+"""
+Shortest maze path.
+"""
+
+
+def read_file(filename: str) -> list[list[int | str]]:
+    """
+    Джія
+    """
+    ...
+
+
+def get_neighbors(row: int, column: int) -> list[tuple[int, int]]:
+    """
+    Богдан
+    """
+    ...
+
+
+def is_valid() -> bool:
+    """
+    Богдан
+    """
+    ...
+
+
+def find_start(matrix: list[tuple[int, int]]) -> tuple[int, int]:
+    """
+    Віктор
+    """
+    ...
+
+
+def get_shortest_path(matrix: list[tuple[int, int]],
+                      start: tuple[int, int]) -> list[tuple[int, int]]:
+    """
+    Марта, Арсеній
+    """
+    ...
+
+
+def visualize_results(shortest_path: list[tuple[int, int]],
+                      matrix: list[tuple[int, int]]):
+    """
+    Віктор
+    """
+    ...
