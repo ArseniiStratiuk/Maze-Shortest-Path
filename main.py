c115a6f30668f3b41201fcfcb02a3f5d72e04283
Arsenii Stratiuk <stratuik.777@gmail.com>
2024-11-21 19:01:02 +0200

    Implement BFS

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def read_file(filename: str) -> list[list[int | str]]:
     ...
 
 
-def get_neighbors(row: int, column: int) -> list[tuple[int, int]]:
+def get_neighbors(matrix: list[tuple[int, int]], row: int, column: int) -> list[tuple[int, int]]:
     """
     Богдан
     """
@@ -43,13 +43,46 @@ def find_start(filename: str) -> tuple[int, int]:
     return None
 
 
+def get_shortest_path(matrix: list[tuple[int, int]], finish: tuple[int, int],
+                      start: tuple[int, int]) -> list[tuple[int, int]] | int:
+    """
+    Find the shortest path in the maze, starting from the start position,
+    implementing the Breadth-First Search algorithm.
 
-def get_shortest_path(matrix: list[tuple[int, int]],
-                      start: tuple[int, int]) -> list[tuple[int, int]]:
+    :param matrix: list[tuple[int, int]], The matrix of the maze, represented
+        as a list of coordinates of the possible paths.
+    :param finish: tuple[int, int], The end position.
+    :param start: tuple[int, int], The start position.
+    :return list[tuple[int, int]], The shortest path from the start to the end position,
+        represented as a list of coordinates of the path. If there is no path, return -1.
+
+    >>> matrix = read_file('input.csv')
+    >>> start = find_start('input.csv')
+    TODO: Add tests.
     """
-    Марта, Арсеній
-    """
-    ...
+    queue = [start]
+    visited = set()
+    parents = {}
+    parents[start] = None
+    visited.add(start)
+
+    while queue:
+        current = queue.pop(0)
+
+        if current == finish:
+            path = []
+            while current is not None:
+                path.append(current)
+                current = parents[current]
+            return path[::-1]
+
+        for neighbor in get_neighbors(matrix, *current):
+            if neighbor not in visited:
+                visited.add(neighbor)
+                parents[neighbor] = current
+                queue.append(neighbor)
+
+    return -1
 
 
 def visualize_results(shortest_path: list[tuple[int, int]],
