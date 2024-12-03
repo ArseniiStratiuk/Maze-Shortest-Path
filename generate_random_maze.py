45c97560181654d26c992f1f9f88e24204e077c5
Arsenii Stratiuk <stratuik.777@gmail.com>
2024-12-03 15:06:55 +0200

    Add iterative version of generating the maze

--- generate_random_maze.py
+++ generate_random_maze.py
@@ -28,34 +28,25 @@ def generate_maze(rows: int, cols: int) -> list[list[int]]:
     if rows < 3 or cols < 3:
         raise ValueError("Розмір лабіринту має бути не менше 3x3")
 
-    def dfs(row_index: int, col_index: int) -> None:
-        """
-        Recursive helper function to carve paths in the maze.
-
-        Args:
-            row (int): The current row position.
-            col (int): The current column position.
-        """
-        directions = [(0, 2), (0, -2), (2, 0), (-2, 0)]
-        random.shuffle(directions)
-
-        for x, y in directions:
-            new_row_index = row_index + x
-            new_col_index = col_index + y
-
-            if 0 <= new_row_index < rows and 0 <= new_col_index < cols:
-                if maze[new_row_index][new_col_index] == 1:
-                    maze[row_index + x // 2][col_index + y // 2] = 0
-                    maze[new_row_index][new_col_index] = 0
-                    dfs(new_row_index, new_col_index)
-
     maze = [[1 for _ in range(cols)] for _ in range(rows)]
-
     start_row = random.randint(0, (rows - 1) // 2) * 2
     start_col = random.randint(0, (cols - 1) // 2) * 2
     maze[start_row][start_col] = 'S'
 
-    dfs(start_row, start_col)
+    directions = [(0, 2), (0, -2), (2, 0), (-2, 0)]
+    stack = [(start_row, start_col)]
+
+    while stack:
+        row, col = stack.pop()
+        random.shuffle(directions)
+        for x, y in directions:
+            new_row = row + x
+            new_col = col + y
+
+            if 0 <= new_row < rows and 0 <= new_col < cols and maze[new_row][new_col] == 1:
+                maze[row + x // 2][col + y // 2] = 0
+                maze[new_row][new_col] = 0
+                stack.append((new_row, new_col))
 
     while True:
         finish_row = random.randint(0, (rows - 1) // 2) * 2
@@ -81,7 +72,8 @@ def save_maze_to_csv(maze: list[list[int]], filename: str):
 
 
 if __name__ == "__main__":
-    rows, cols = 47, 47  # Maze dimensions (should be odd)
+    rows, cols = 10001, 10001  # Maze dimensions (should be odd)
+
     maze = generate_maze(rows, cols)
     save_maze_to_csv(maze, "maze.csv")
     print("Maze is saved to 'maze.csv'")
