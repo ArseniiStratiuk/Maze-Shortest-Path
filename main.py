6b052f45d4a2770b9e5b155a7e1d5876b495962a
MartaKosiv <kosiv.pn@ucu.edu.ua>
2024-12-05 10:27:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,123 @@
 """
-Shortest maze path.
+This module contains functions for reading a matrix from a .csv file, finding
+the shortest path in the maze, and visualizing the results.
+
+The matrix is represented as a list of rows, where each row is a list of
+integers and strings. The integers represent the obstacles (1) and the
+free spaces (0), while the strings represent the start ('S') and the
+finish ('F') positions.
 """
+STEPS = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
 
 def read_file(filename: str) -> list[list[int | str]]:
     """
-    Джія
+    Reads the matrix from a .csv file and returns a list of rows.
+    Converts '1' and '0' to integers, keeps 'S' and 'F' as strings, 
+    and returns 'incorrect matrix' for invalid inputs.
+
+    Args:
+        file_name (str): .csv file with the matrix.
+
+    Returns:
+        list[list[int | str]]: list of rows or 'Incorrect matrix' if invalid inputs are found.
+
+    >>> with open("matrix1.csv", "w", encoding="utf-8") as f:
+    ...    _ = f.write("0,1,0,0,0\\n0,1,0,1,0\\n0,0,0,1,F\\nS,1,1,1,0\\n0,0,0,0,0\\n")
+    >>> read_file('matrix1.csv')
+    [[0, 1, 0, 0, 0], [0, 1, 0, 1, 0], [0, 0, 0, 1, 'F'], ['S', 1, 1, 1, 0], [0, 0, 0, 0, 0]]
+    >>> with open("matrix2.csv", "w", encoding="utf-8") as f:
+    ...    _ = f.write("0,S,0,0\\n1,1,0,F\\n0,S,0,0\\n")
+    >>> read_file('matrix2.csv')
+    'Incorrect maze'
     """
-    ...
+    matrix=[]
+    found_s_f = False
+    count_s_f=0
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            row=line.strip().split(',')
+            processed_row = []
+            for part in row:
+                if part == '1':
+                    processed_row.append(1)
+                elif part == '0':
+                    processed_row.append(0)
+                elif part in {'S', 'F'}:
+                    count_s_f+=1
+                    processed_row.append(part)
+                else:
+                    return 'Incorrect maze'
+            if 'S' in processed_row and 'F' in processed_row:
+                found_s_f = True
+            matrix.append(processed_row)
+    if not found_s_f and count_s_f!=2:
+        return 'Incorrect maze'
+    return matrix
 
 
-def get_neighbors(row: int, column: int) -> list[tuple[int, int]]:
+def get_neighbors(matrix: list[list[int | str]],
+                  row: int, column: int) -> list[tuple[int, int]]:
     """
-    Богдан
+    Get the neighbors of the given row and column in the matrix.
+
+    Returns:
+        list[tuple[int, int]]: The neighbors of the given row and column.
+
+    >>> get_neighbors([[1, 1, 1, 1, 1], [1, 0, 0, 1, 1], [1, 0, 1, 0, 1], \
+[1, 1, 1, 1, 1], [1, 0, 0, 0, 1]], 1, 1)
+    [(2, 1), (1, 2)]
     """
-    ...
+    result_lst = []
+    for y, x in STEPS:
+        y += row
+        x += column
+        if is_valid(matrix, y, x) and (matrix[y][x] in [0, 'F']):
+            result_lst.append((y, x))
+    return result_lst
 
 
-def is_valid() -> bool:
+def is_valid(matrix: list[list[int | str]], row: int, column: int) -> bool:
     """
-    Богдан
+    Check if the given row and column are valid in the matrix.
+
+    Returns:
+        bool: True if the row and column are valid, False otherwise.
+
+    >>> is_valid([[1, 1, 1, 1, 1], [1, 0, 0, 1, 1], [1, 0, 1, 0, 1], \
+[1, 1, 1, 1, 1], [1, 0, 0, 0, 1]], 1, 1)
+    True
     """
-    ...
+    return 0 <= row < len(matrix) and 0 <= column < len(matrix[0])
 
 
-def find_start(matrix: list[tuple[int, int]]) -> tuple[int, int]:
+def find_start(matrix: list[list[int | str]]) -> tuple[int, int]:
     """
-    Віктор
+    Find the start position in the maze.
+
+    Args:
+        matrix (list[list[int | str]]): The maze represented as a matrix.
+
+    Returns:
+        tuple[int, int]: The start position.
+
+    >>> maze = [
+    ...     [ 0 , 1 , 0 , 0 , 0 ],
+    ...     [ 0 , 1 , 0 , 1 , 0 ],
+    ...     [ 0 , 0 , 0 , 1 ,'F'],
+    ...     ['S', 1 , 1 , 1 , 0 ],
+    ...     [ 0 , 0 , 0 , 0 , 0 ],
+    ... ]
+    >>> find_start(maze)
+    (3, 0)
     """
-    ...
+    n = len(matrix)
+    for i in range(n):
+        m = len(matrix[i])
+        for j in range(m):
+            if matrix[i][j] == 'S':
+                return (i, j)
+    return None
 
 
 def get_shortest_path(matrix: list[tuple[int, int]],
@@ -70,27 +159,24 @@ def get_shortest_path(matrix: list[tuple[int, int]],
     >>> len(get_shortest_path(maze, start)) == 8
     True
     """
-    def recurtione(node):
-        neighbors = get_neighbors(node[0], node[1], matrix)
+    def recurtione(queue_, visited):
+        node = queue_.pop()
+        neighbors = get_neighbors(matrix, node[0], node[1])
+
         for neighbor in neighbors:
             if neighbor in visited:
                 continue
-            queue.append(neighbor)
+            queue_.append(neighbor)
             visited.add(neighbor)
-            node = queue.pop(0)
             if matrix[neighbor[0]][neighbor[1]] == 'F':
                 return [neighbor]
-            return [neighbor] + recurtione(neighbor)
+            return [neighbor] + recurtione(queue_, visited)
 
-    queue = []
+    queue = [start]
     visited = set()
-    path = [start] + recurtione(start)
+    path = [start] + recurtione(queue, visited)
     return path
 
-
-def visualize_results(shortest_path: list[tuple[int, int]],
-                      matrix: list[tuple[int, int]]):
-    """
-    Віктор
-    """
-    ...
+if __name__ == '__main__':
+    import doctest
+    print(doctest.testmod())
